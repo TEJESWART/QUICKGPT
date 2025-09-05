0a81c69619d0c1e5ff1ffce9b9d663520bc12f91
Tejeswar T <tejeswar@example.com>
2025-09-05 19:18:05 +0530

    first commit

--- server/routes/creditRoutes.js
+++ server/routes/creditRoutes.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import { getPlans, purchasePlan } from '../controllers/creditcontroller.js';
-import { protect } from '../middlewares/auth.js';
+import express from "express";
+import { getPlans, purchasePlan } from "../controllers/creditController.js";
+import { protect } from "../middlewares/auth.js";
 
-const creditRouter = express.Router()
+const creditRouter = express.Router();
 
-creditRouter.get('/plan', getPlans)
-creditRouter.post('/purchase', protect, purchasePlan)
+// Routes
+creditRouter.get("/plan", getPlans);
+creditRouter.post("/purchase", protect, purchasePlan);
 
-export default creditRouter
+export default creditRouter;
